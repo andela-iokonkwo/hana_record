189254da6dca37e46839051aea22d6022bebbd93
Ikem Okonkwo <ikem.okonkwo@andela.com>
2015-12-17 09:43:49 +0100

    create an executable hana that encapsulate g and migrate command as subcommand

--- lib/hana_record/cli/application.rb
+++ lib/hana_record/cli/application.rb
@@ -7,7 +7,8 @@ require_relative "generators/g"
 module HanaRecord
   module CLI
     class Application < Thor
-
+      register HanaRecord::CLI::G, 'g', 'g [model, migration]', 'Executes a multi-step subtask'
+      register HanaRecord::CLI::Migrate, 'migrate', 'migrate', 'Executes a multi-step subtask'
     end
   end
 end
