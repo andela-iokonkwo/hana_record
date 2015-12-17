e0eae63499b130d153a64b8a581bf3af5faecca1
Ikem Okonkwo <ikem.okonkwo@andela.com>
2015-12-17 09:32:52 +0100

    set up orm for creating generators

---
+++ lib/hana_record/cli/application.rb
@@ -0,0 +1,13 @@
+require "thor"
+require "time"
+require_relative "generators/migrate"
+require_relative "generators/model"
+require_relative "generators/g"
+
+module HanaRecord
+  module CLI
+    class Application < Thor
+
+    end
+  end
+end
