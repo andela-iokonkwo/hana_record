fac9215a7e7496bcdcdf085cb49d483c14717cff
Ikem Okonkwo <ikem.okonkwo@andela.com>
2015-12-17 09:45:14 +0100

    add all dependency in the right order

--- lib/hana_record.rb
+++ lib/hana_record.rb
@@ -1,3 +1,10 @@
+require "hana_record/utils"
+require "hana_record/attributes"
+require "hana_record/base"
+require "hana_record/table"
+require "hana_record/migrations"
+require "hana_record/schema"
+require "hana_record/cli/application"
 require "hana_record/version"
 
 module HanaRecord
